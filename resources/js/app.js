43b737be0cb9db59c357e1de3ad71dd0797070cd
ret123 <rathish83@gmail.com>
2019-05-18 10:25:01 +0530

    Vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,10 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Vue from 'vue';    
+import Vuetify from 'vuetify';
+Vue.use(Vuetify); 
+import router from './Router/router';
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +23,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('AppHome', require('./components/AppHome.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +33,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router,
 });
---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <Toolbar></Toolbar>
+        <app-footer></app-footer>
+    </div>
+   
+</template>
+
+<script>
+import Toolbar from "./Toolbar";
+import AppFooter from './AppFooter';
+
+export default {
+    components : {
+    Toolbar,
+    AppFooter
+    },
+    mounted() {
+        console.log('component mounted');
+    }
+}
+</script>
+   
+<style>
+
+</style>
